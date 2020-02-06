a99a2b3f35c3071a740473a95be7102ba3e3e678
Dhwin Alhambra <j.darwin.j.alhambra@accenture.com>
2020-02-06 08:29:41 +0800

    Project 1 changes

--- SRC/COBOL1.cbl
+++ SRC/COBOL1.cbl
@@ -5,3 +5,4 @@ Push topic to dev branch
 Edit code in feature branch 1/29 10:36pm
 Edit code in bug fix branch 1/30 1:40pm
 Edit code in Hot fix branch 1/31 11:21am
+Edit code in Project1 branch 2/6 8:27am
--- SRC/COBOL2.cbl
+++ SRC/COBOL2.cbl
@@ -1,1 +1,2 @@
 Initial code for Cobol2.
+Edit code for Project1 branch 2/6 8:28am
